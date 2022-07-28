aefa3b1d2c112a79eac80fe98e447e8dbd0bf77b
Tobias <tobiasaboh@gmail.com>
2022-07-28 04:52:06 -0700

    Added dynamic size and tap to focus

--- flutter_application_1/lib/my_scroll_component.dart
+++ flutter_application_1/lib/my_scroll_component.dart
@@ -6,19 +6,18 @@ class ScrollComponent extends StatefulWidget {
   List<Widget> listItems; // list of widgets
   Color backgroundColor; // background color of the widget
   Color color; // color of non active items
-  Color activeColor; // color of current active item 
+  double dynamicItemOpacity; // color of current active item 
   double minHeight; // min height for each row
   double maxHeight; // max height for each row
   double height; // hieght of the outer container
-  ScrollComponent({required this.listItems, this.backgroundColor = Colors.white, this.color = Colors.grey, this.activeColor = Colors.blueGrey, this.height = 200,  this.minHeight = 50, required this.maxHeight});
+  ScrollComponent({required this.listItems, this.backgroundColor = Colors.white, this.color = Colors.grey, this.dynamicItemOpacity = 0.8, this.height = 200,  this.minHeight = 50, required this.maxHeight});
   
   @override
   State<ScrollComponent> createState() => _ScrollComponentState();
 }
 
-class _ScrollComponentState extends State<ScrollComponent> {
+class _ScrollComponentState extends State<ScrollComponent> with SingleTickerProviderStateMixin{
   int _focusedIndex = 0;
-
   void _onFocusedItem(int index){
     setState(() {
       _focusedIndex = index;
@@ -29,12 +28,12 @@ class _ScrollComponentState extends State<ScrollComponent> {
   Widget _buildListItems(BuildContext context, int index){
     return Container(
           constraints: BoxConstraints(
-            minHeight: _focusedIndex == index ? this.widget.minHeight + 30 : this.widget.minHeight,
+            minHeight: this.widget.minHeight, //+ 30 : this.widget.minHeight,
             maxHeight: this.widget.maxHeight,
           ),
           child: Card(  
             elevation: _focusedIndex == index ? 20 : null,
-            color: _focusedIndex == index ? this.widget.activeColor : this.widget.color,
+            color: this.widget.color,
             child: Center(
               heightFactor: 1,
               child: this.widget.listItems[index],)
@@ -43,6 +42,7 @@ class _ScrollComponentState extends State<ScrollComponent> {
   }
   @override
   Widget build(BuildContext context) {
+    
     return Container(
       decoration: BoxDecoration(
         color: this.widget.backgroundColor,
@@ -54,6 +54,10 @@ class _ScrollComponentState extends State<ScrollComponent> {
         padding: EdgeInsets.all(6),
         child: Container(
           child: ScrollSnapList(
+            duration: 200,
+            dynamicItemSize: true,
+            focusOnItemTap: true,
+            dynamicItemOpacity: this.widget.dynamicItemOpacity,
             scrollDirection: Axis.vertical,
             itemSize: this.widget.minHeight,
             itemCount: this.widget.listItems.length,
